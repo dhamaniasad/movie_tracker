13fa7245f0ed593dafe07af16a5f9f9f85b38a69
Asad Dhamani <dhamaniasad@gmail.com>
2015-09-09 22:21:33 +0530

    Fetch data from omdb

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
-var OMDbBaseUrl = "http://www.omdbapi.com/?";
+// Imports
+var got = require('got');
 
+const OMDbBaseUrl = "http://www.omdbapi.com/?s=";
 
 $("#searchForm").submit(function () {
     search = $('input[name=movie-name]').val();
+    got(OMDbBaseUrl + search, {json: true}, function (err, data, res) {
+        if (err) {
+            alert('Your internet connection or the database seems to be down :(. Try again in a little while.')
+        }
+        else {
+            // There must be a better way to do this
+            console.log(data);
+            updateCard(data);
+        }
+    });
     // Do not want the page to reload on submit
     return false;
 });
+
+function updateCard(data) {
+    $('#movieCard').css('display', 'flex').find('.header').text(data.Search[0].Title);
+}
