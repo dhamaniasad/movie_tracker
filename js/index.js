fba966a2af08c6b5343c038e757de2c76ae83bd6
Asad Dhamani <dhamaniasad@gmail.com>
2015-09-10 15:52:37 +0530

    Add basic ViewModel for search

--- js/index.js
+++ js/index.js
@@ -4,36 +4,29 @@ var got = require('got');
 const OMDbBaseUrl = "http://www.omdbapi.com/?s=";
 
 $("#searchForm").submit(function () {
-    search = $('input[name=movie-name]').val();
+    // Do not want the page to reload on submit
+    return false;
+});
+
+function fetchData (search) {
     got(OMDbBaseUrl + search, {json: true}, function (err, data, res) {
         if (err) {
             alert('Your internet connection or the database seems to be down :(. Try again in a little while.')
         }
         else {
-            // There must be a better way to do this
-            console.log(data);
-            updateCard(data);
+            return data
         }
     });
-    // Do not want the page to reload on submit
-    return false;
-});
-
-function updateCard(data) {
-    $('#movieCard').css('display', 'flex').find('.header').text(data.Search[0].Title);
 }
 
 // Knockout
 
-// Here's my data model
-var ViewModel = function(first, last) {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
+var SearchVM = function (first) {
+    this.movieInput = ko.observable(first);
 
-    this.fullName = ko.pureComputed(function() {
-        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-        return this.firstName() + " " + this.lastName();
+    this.movieName = ko.pureComputed(function() {
+        return this.movieInput();
     }, this);
 };
 
-ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
+ko.applyBindings(new SearchVM('2001: A Space Odyssey'));
