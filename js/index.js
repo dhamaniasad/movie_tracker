9f9165c318ea25083b88b367139629dfa357f85f
Asad Dhamani <dhamaniasad@gmail.com>
2015-09-10 13:33:37 +0530

    Add KO

--- js/index.js
+++ js/index.js
@@ -22,3 +22,18 @@ $("#searchForm").submit(function () {
 function updateCard(data) {
     $('#movieCard').css('display', 'flex').find('.header').text(data.Search[0].Title);
 }
+
+// Knockout
+
+// Here's my data model
+var ViewModel = function(first, last) {
+    this.firstName = ko.observable(first);
+    this.lastName = ko.observable(last);
+
+    this.fullName = ko.pureComputed(function() {
+        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
+        return this.firstName() + " " + this.lastName();
+    }, this);
+};
+
+ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
